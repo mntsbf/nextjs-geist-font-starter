81d2330778edb84301a0a546ce16c428de5c0e03
mntsbf <66760711+mntsbf@users.noreply.github.com>
2025-08-14 10:28:16 -0400

    Add .next/static/chunks/src_app_dashboard_clients_[id]_page_tsx_25cb88fa._.js 1755181694616

---
+++ .next/static/chunks/src_app_dashboard_clients_[id]_page_tsx_25cb88fa._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_a54046fa._.js",
+  "static/chunks/node_modules_6f4a708d._.js"
+],
+    source: "dynamic"
+});
